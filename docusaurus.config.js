7217e7314b9cba00ca5f343657479108c48f4711
hgedia <haresh.gedia@gmail.com>
2023-05-29 14:06:42 +0100

    Test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Scratch place',
-  tagline: 'Dinosaurs are cool',
+  title: 'Scratch places',
+  tagline: 'BYOB',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
